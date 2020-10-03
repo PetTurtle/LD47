6d966ca129057196af363843d3d39b295da496c6
PetTurtle <petturtledev@gmail.com>
2020-10-02 20:18:58 -0500

    Bullet Timeout

--- Game/Bullet/Bullet.gd
+++ Game/Bullet/Bullet.gd
@@ -6,3 +6,7 @@ export(float) var move_speed: float = 100
 
 func _physics_process(delta) -> void:
 	position += (Vector2.RIGHT * move_speed * delta).rotated(rotation)
+
+
+func _on_DeleteTimer_timeout():
+	queue_free()
